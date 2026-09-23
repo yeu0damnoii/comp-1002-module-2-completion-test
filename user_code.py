c94ff2d532fd4d838551ae664c22be8644d60bae
Duc Huy <duchuyn1604@gmail.com>
2026-09-23 16:15:15 +0930

    def function

--- user_code.py
+++ user_code.py
@@ -1,24 +1,32 @@
-outfile = open("averages.txt", "w")
-infile = open("scores.txt", "r")
-lis= infile.readlines()
-for line in lis:
-    student = line.split(",")
+
+
+def calculate_average(scores:list):
     total = 0
     count = 0
-    for i in student[1:]:
+    for i in scores:
         i=int(i)
         count +=1
         total+=i
-    if count == 0 or total ==0:
-        continue
-    else:
-        print(count)
-        print(total)
-        print(round(total/count,1))
-        print()
+    if total > 0:
+        return (round(total/count,1))
+    
 
+def process_scores(in_filename="scores.txt", out_filename="averages.txt"):
+    outfile = open("averages.txt", "w")
+    infile = open("scores.txt", "r")
+    #lis= infile.readlines()
+    averages = []
+    for line in infile:
+        line=line.strip()
+        if not line:
+            continue
+        scores = line.split(",")
+        avg = calculate_average(scores[1:])
+        outfile.write(f"{avg}\n")
+        averages.append(avg)
+    outfile.close()
+    infile.close()
+    return averages
 
-outfile.close()
-infile.close()
 
 
