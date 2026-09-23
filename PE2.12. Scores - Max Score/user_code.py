9156e6feac2d58cf6d12cd885731e547bc83097b
Duc Huy <duchuyn1604@gmail.com>
2026-09-23 17:41:26 +0930

    full mark

--- PE2.12. Scores - Max Score/user_code.py
+++ PE2.12. Scores - Max Score/user_code.py
@@ -1,2 +1,31 @@
 def maximum_score(scores):
-    
+    scores_int = []
+    for score in scores:
+        scores_int.append(int(score))
+    return max(scores_int)
+
+
+
+def process_scores(in_filename="scores.txt", out_filename="maximums.txt"):
+    outfile = open("maximums.txt", "w")
+    infile = open("scores.txt", "r")
+
+    maximum_scores = []
+    maximum_of_maximum_scores = []
+    for line in infile:
+        line=line.strip()
+        if line:
+            student_score = line.split(',')
+            maximum=maximum_score(student_score[1:])
+            maximum_scores .append([student_score[0], maximum])
+            maximum_of_maximum_scores.append(maximum)
+    outfile.write(f'{max(maximum_of_maximum_scores)}\n')
+    for item in maximum_scores:
+        line = item[0] + ',' + str(item[1]) + '\n'
+        outfile.write(line)
+
+    return maximum_scores 
+
+process_scores()
+        
+
